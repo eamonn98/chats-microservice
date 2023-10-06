e35a88b04b4490725be8055fa94acd34db487fe3
eamonn98 <eamonn98@gmail.com>
2023-10-06 15:57:16 +0000

    Updated cypher queries

--- src/plugins/databaseAdapter.ts
+++ src/plugins/databaseAdapter.ts
@@ -2,7 +2,13 @@ import fp from 'fastify-plugin'
 import neo4j from 'neo4j-driver';
 
 export interface DatabaseAdapterPluginOptions {
-    // Specify DatabaseAdapter plugin options here
+  // Specify DatabaseAdapter plugin options here
+}
+
+interface Neo4jResult {
+  keys: any[],
+  records: any[],
+  summary: any
 }
 
 const URI = 'neo4j+s://5aa1b79c.databases.neo4j.io';
@@ -11,65 +17,99 @@ const PASSWORD = 'Xc4sMnRvQGKZmQ-qOn_TSzKPxJUhWWFmaqgk3L7ZCuQ';
 let driver: any;
 
 const connect = async () => {
-    try {
-        driver = neo4j.driver(URI, neo4j.auth.basic(USERNAME, PASSWORD));
-        const serverInfo = await driver.getServerInfo();
-        console.log('Connected ' + serverInfo);
-    } catch (err) {
-        console.error(err);
+  try {
+    driver = neo4j.driver(URI, neo4j.auth.basic(USERNAME, PASSWORD));
+    const serverInfo = await driver.getServerInfo();
+    console.log('Connected ' + serverInfo);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const parseNeo4jObject = (neo4jObject: Neo4jResult) => {
+  const parsedObject: any[] = [];
+
+  for (const record of neo4jObject.records) {
+    const recordObj = {};
+    for (const keyIndex in neo4jObject.keys) {
+      const key = neo4jObject.keys[keyIndex];
+      recordObj[key] = record['_fields'][keyIndex];
     }
+
+    parsedObject.push(recordObj);
+  }
+
+  return parsedObject;
 }
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<DatabaseAdapterPluginOptions>(async (fastify, opts) => {
+  
+  fastify.decorate('sendChat', async function(user: string, room: string, message: string): Promise<void> {
     connect();
-    fastify.decorate('sendChat', async function (user: string, room: string, message: string): Promise<void> {
-        const cqlQuery = 'MERGE (r:Room) { name: "$room"}) \
-            CREATE (m:Chat {message: $message, created: datetime()}) \
-            \
-            MATCH (u:User {username: $user})\
-            CREATE (m)-[r1:CONTAINS]->(m) \
-            CREATE (m)[r2:SENT_BY]->(u) \
-            \
-        ';
-        await driver.executeQuery(
-            cqlQuery,
-            { user, room, message },
-            { database: 'neo4j' }
-        );
+    const cqlQuery = `
+    MERGE (r:Room { name: $room});
 
-        await driver.close();
-    })
+    MATCH (r:Room {name: $room})
+    MATCH (u:User {username: $username})
+    CREATE (r)-[r1:CONTAINS]->(m:Chat {message: $message, created: datetime()})-[r2:SENT_BY]->(u);
+    `;
+    console.log(`cqlQuery: ${cqlQuery}`);
+    await driver.executeQuery(
+      cqlQuery,
+      { user, room, message },
+      { database: 'neo4j' }
+    );
 
-    fastify.decorate('getChats', async function (room: string) {
-        connect();
-        const cqlQuery = 'MATCH (r:Room) {name: $room})-[:CONTAINS]->(m:Message) RETURN m ';
-        const result = await driver.executeQuery(
-            cqlQuery,
-            { room },
-            { database: 'neo4j' }
-        );
-        await driver.close();
+    await driver.close();
+  })
 
-        return result;
-    });
+  fastify.decorate('getChats', async function(room: string) {
+    connect();
+    const cqlQuery = 'MATCH (r:Room {name: $room})-[c:CONTAINS]->(m:Chat)-[s:SENT_BY]->(u) RETURN m.message AS message, apoc.date.toISO8601(m.createdAt.epochMillis, "ms") AS createdAt, u.username AS username;';
+    const result = await driver.executeQuery(
+      cqlQuery,
+      { room },
+      { database: 'neo4j' }
+    );
+    await driver.close();
 
-    fastify.decorate('createUser', function (user: string, password: string, email: string) {
-        return false
-    });
+    const chats = parseNeo4jObject(result);
 
-    fastify.decorate('authenticateUser', function (user: string, password: string) {
-        return false;
-    });
+    return chats;
+  });
+
+  fastify.decorate('createUser', function(user: string, password: string, email: string) {
+    return false
+  });
+
+  fastify.decorate('authenticateUser', function(user: string, password: string) {
+    return false;
+  });
+
+
+  fastify.decorate('runCypher', async function(cypher: string) {
+    connect();
+    const result = await driver.executeQuery(
+      cypher,
+      {},
+      { database: 'neo4j' }
+    );
+    await driver.close();
+
+    return { parsedResult: parseNeo4jObject(result), cypherResponse: result };
+  });
 })
 
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
-    export interface FastifyInstance {
-        sendChat(user: string, room: string, message: string): void;
-        getChats(room: string): unknown;
-        createUser(user: string, password: string, email: string): boolean;
-        authenticateUser(user: string, password: string): boolean;
-    }
+  export interface FastifyInstance {
+    sendChat(user: string, room: string, message: string): void;
+    getChats(room: string): unknown;
+    createUser(user: string, password: string, email: string): boolean;
+    authenticateUser(user: string, password: string): boolean;
+
+    runCypher(cypher: string): any;
+  }
 }
--- src/routes/chat/index.ts
+++ src/routes/chat/index.ts
@@ -1,17 +1,21 @@
 import { FastifyPluginAsync } from "fastify"
 
 interface IQuerystring {
-    room: string;
-  }
+  room: string;
+}
 
-  interface IHeaders {
-    
-  }
-
+interface IHeaders {
   
-  interface IReply {
-    200: { chats: any };
-  }
+}
+
+
+interface IReply {
+  200: { chats: any };
+}
+
+interface IBody {
+  cypher: string
+}
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get<{
@@ -20,8 +24,17 @@ const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
     Reply: IReply
   }>('/', async (request, reply) => {
     const { room } = request.query
-    reply.code(200).send({chats: fastify.getChats(room)});
-  })
+    reply.code(200).send({chats: await fastify.getChats(room)});
+  });
+
+  fastify.post<{Body: IBody}>('/', async (request, reply) => {
+    const cypher = request.body.cypher;
+    console.log(cypher);
+
+    const res = await fastify.runCypher(cypher);
+
+    reply.code(200).send({resp: res});
+  });
 }
 
     // const room = request.body['room'];
